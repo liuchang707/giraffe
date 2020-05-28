7274fa3f96be4485259b0b739476b34ee292e313
liuchang707 <49985842+liuchang707@users.noreply.github.com>
2020-05-28 11:41:19 +0800

    Add files via upload

---
+++ 03.gene_family/cafetutorial_run1.sh
@@ -0,0 +1,7 @@
+#!/public/home/yangjie/software/CAFE/release/cafe
+load -i filtered_cafe_input.txt -t 8 -l reports/log_run1.txt
+tree (((horse:62.9,dog:62.9):1.418423,(camel:54.308601,(pig:52.107035,(whale:47,((pronghorn:19.452816,(giraffe:10.092345,okapi:10.092345):9.360471):1.010529,(whitelippeddeer:19.449153,(forestmuskdeer:18,cattle:18):1.449152):1.014192):26.5367):5.107035):2.201567):10.009822):35.681577,human:100);
+
+lambda -s
+report reports/report_run1
+
